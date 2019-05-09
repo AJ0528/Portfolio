331cbb3c98305c2ab7b8b5f4faa995c5d1030773
Alfredo <puertorocat@gmail.com>
2019-05-09 10:12:46 -0400

    new changes

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
+    var elems = document.querySelectorAll(".fixed-action-btn");
     var instances = M.FloatingActionButton.init(elems, options);
   });
+
+  // Or with jQuery
+ 
